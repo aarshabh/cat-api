e7d38e94531855aed891dda2dd2dedf7f41e1c6e
Aarshabh Agrawal <55677141+aarshabh@users.noreply.github.com>
2025-10-23 21:24:43 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Cats from './components/Cats.jsx'
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1> Cat List </h1>
+      </div>
+      
+      <Cats/>
+
+    </>
+  )
+}
+
+export default App
